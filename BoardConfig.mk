a40b559086ee893d50ddbcc263d3177a6e66f5ab
Noel Macwan <yulespencer@gmail.com>
2014-05-14 20:26:32 +0530

    tianchi: update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,20 +13,43 @@
 # limitations under the License.
 
 # inherit from the common rhine definitions
--include device/sony/rhine-common/BoardConfigCommon.mk
+include device/sony/rhine-common/BoardConfigCommon.mk
 
 # inherit from the proprietary version
-#-include vendor/sony/honami/BoardConfigVendor.mk
+include vendor/sony/tianchi/BoardConfigVendor.mk
+# inherit from Sony common
+
+include device/sony/common/BoardConfigCommon.mk
+
+# inherit from qcom-common
+include device/sony/qcom-common/BoardConfigCommon.mk
+
+USE_CAMERA_STUB := false
+
+TARGET_BOARD_PLATFORM := msm8960
+TARGET_CPU_VARIANT := krait
+BOARD_VENDOR_PLATFORM := tianchi
+TARGET_BOOTLOADER_BOARD_NAME := qcom
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := L39h,C6902,C6903,C6906,C6843,honami
+TARGET_OTA_ASSERT_DEVICE := d5322, xm50h,tianchi
 
-TARGET_SPECIFIC_HEADER_PATH += device/sony/honami/include
+TARGET_SPECIFIC_HEADER_PATH += device/sony/tianchi/include
 
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/honami/bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/tianchi/bluetooth
 
 # Kernel properties
-TARGET_KERNEL_CONFIG := cm_rhine_honami_row_defconfig
+TARGET_KERNEL_SOURCE := kernel/sony/msm8928
+TARGET_KERNEL_CONFIG := yukon_tianchi_dsds_defconfig
+
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_QCOM_AUDIO_VARIANT := caf
+TARGET_QCOM_DISPLAY_VARIANT := caf
+TARGET_QCOM_MEDIA_VARIANT := caf
+TARGET_USES_QCOM_BSP := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_BSP
+TARGET_DISPLAY_USE_RETIRE_FENCE := true
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 26
@@ -35,3 +58,73 @@ BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01400000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01400000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1056964608
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
+
+BOARD_USES_ALSA_AUDIO := true
+BOARD_USES_LEGACY_ALSA_AUDIO := true
+TARGET_USES_QCOM_COMPRESSED_AUDIO := true
+
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+
+USE_DEVICE_SPECIFIC_CAMERA := true
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
+COMMON_GLOBAL_CFLAGS += -DSONY_CAM_PARAMS
+
+# Wlan
+BOARD_HAS_QCOM_WLAN              := true
+BOARD_HAS_QCOM_WLAN_SDK          := true
+BOARD_WLAN_DEVICE                := qcwcn
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME          := "wlan"
+WIFI_DRIVER_FW_PATH_STA          := "sta"
+WIFI_DRIVER_FW_PATH_AP           := "ap"
+
+BOARD_HAVE_BLUETOOTH_QCOM := true
+BLUETOOTH_HCI_USE_MCT := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/tianchi/bluetooth
+
+TARGET_RECOVERY_FSTAB = device/sony/tianchi/rootdir/root/fstab.qcom
+
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+BOARD_CUSTOM_BOOTIMG_MK := device/sony/tianchi/custombootimg.mk
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/sony/tianchi/recovery/recovery_keys.c
+BOARD_CUSTOM_GRAPHICS := ../../../device/sony/tianchi/recovery/graphics.c
+
+BOARD_USES_QC_TIME_SERVICES := true
+
+TARGET_POWERHAL_NO_TOUCH_BOOST := true
+
+# Override healthd HAL
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.qcom
+
+TARGET_POWERHAL_VARIANT := qcom
+
+BOARD_SEPOLICY_DIRS += \
+    device/sony/tianchi/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    app.te \
+    bluetooth.te \
+    device.te \
+    domain.te \
+    drmserver.te \
+    file.te \
+    hci_init.te \
+    healthd.te \
+    init.te \
+    init_shell.te \
+    keystore.te \
+    kickstart.te \
+    mediaserver.te \
+    netd.te \
+    rild.te \
+    surfaceflinger.te \
+    system.te \
+    ueventd.te \
+    wpa_supplicant.te
